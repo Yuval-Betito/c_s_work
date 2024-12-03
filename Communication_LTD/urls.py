ac612294e89a8224c3804e9b3fe6af40975ce07d
Yuval-Betito <yuval36811@gmail.com>
2024-12-03 11:44:52 +0200

    Update urls.py

--- Communication_LTD/urls.py
+++ Communication_LTD/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views  # Import for built-in views
+from users import views  # Import for home view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +10,8 @@ urlpatterns = [
     # Add paths for password change
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
+
+    # Add path for the home page
+    path('', views.home, name='home'),  # Home page route
 ]
+
