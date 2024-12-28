a55e40a4c33a96a463ee6501c15dfdcf7fddb594
Yuval-Betito <yuval36811@gmail.com>
2024-12-29 00:17:41 +0200

    Update models.py

--- users/models.py
+++ users/models.py
@@ -29,6 +29,7 @@ class User(AbstractBaseUser):
     email = models.EmailField(max_length=100, unique=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    reset_token = models.CharField(max_length=100, blank=True, null=True)  # שדה לטוקן לאיפוס סיסמה
 
     objects = UserManager()
 
@@ -69,3 +70,4 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"{self.firstname} {self.lastname}"
+
