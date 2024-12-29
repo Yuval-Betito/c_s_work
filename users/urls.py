e490dae8695beccd00d3622dcd23e5604ba959cb
Yuval-Betito <yuval36811@gmail.com>
2024-12-29 02:00:56 +0200

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import register, user_login, home, create_customer  # ייבוא הפונקציות מ-views
 
+# URLs עבור האפליקציה users
 urlpatterns = [
-    path('register/', register, name='register'),  # נתיב להרשמה
-    path('login/', user_login, name='login'),  # נתיב לכניסה למערכת
-    path('home/', home, name='home'),  # דף הבית
+    path('register/', register, name='register'),  # נתיב לרישום משתמשים חדשים
+    path('login/', user_login, name='login'),      # נתיב לכניסה למערכת
+    path('home/', home, name='home'),              # דף הבית
     path('customer/add/', create_customer, name='add_customer'),  # הוספת לקוח חדש
 ]
