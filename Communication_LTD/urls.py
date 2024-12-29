2899176a67765272d3d256c4bea642e3add9e637
Yuval-Betito <yuval36811@gmail.com>
2024-12-29 17:38:52 +0200

    Update urls.py

--- Communication_LTD/urls.py
+++ Communication_LTD/urls.py
@@ -6,7 +6,7 @@ from users import views  # Import for home view
 urlpatterns = [
     path("admin/", admin.site.urls),  # נתיב לפאנל הניהול של Django
     path('', include('users.urls')),  # חיבור ל-urls של האפליקציה users (הנתיב הראשי)
-    
+
     # נתיבים לשינוי סיסמה
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
@@ -16,4 +16,5 @@ urlpatterns = [
     path('reset_password/', views.reset_password, name='reset_password'),
 
     # נתיב להתנתקות
-    path('logout/', auth_views.Log
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # התנתקות עם הפניה למסך כניסה
+]
