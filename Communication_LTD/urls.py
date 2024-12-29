d95339cd4aae3a05e2d8a26c54ebf1cdecde6b42
Yuval-Betito <yuval36811@gmail.com>
2024-12-29 17:37:41 +0200

    Update urls.py

--- Communication_LTD/urls.py
+++ Communication_LTD/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from users import views  # Import for views in users app
+from django.contrib.auth import views as auth_views  # Import this to fix the issue
+from users import views  # Import for home view
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # נתיב לפאנל הניהול של Django
     path('', include('users.urls')),  # חיבור ל-urls של האפליקציה users (הנתיב הראשי)
-
+    
     # נתיבים לשינוי סיסמה
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
@@ -15,5 +16,4 @@ urlpatterns = [
     path('reset_password/', views.reset_password, name='reset_password'),
 
     # נתיב להתנתקות
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # התנתקות עם הפניה למסך כניסה
-]
+    path('logout/', auth_views.Log
