f8ab04f8fe964af1155c55211dda59f62f2325c6
Yuval-Betito <yuval36811@gmail.com>
2024-12-30 21:16:47 +0200

    Update urls.py

--- Communication_LTD/urls.py
+++ Communication_LTD/urls.py
@@ -1,21 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from users import views  # Import for home view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),  # Admin path
-    path('', include('users.urls')),  # Including app's URLs
-
-    # Paths for password change
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
-
-    # Forgot password and reset password paths
-    path('forgot_password/', views.forgot_password, name='forgot_password'),
-    path('reset_password/', views.reset_password, name='reset_password'),
-
-    # Logout path
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # Logout redirects to login
-    path('login/', views.user_login, name='login'),  # Updated login path
+    path('admin/', admin.site.urls),  # נתיב לפאנל הניהול של Django
+    path('', include('communication.urls')),  # כולל את הנתיבים של אפליקציית ה-communication
+    path('users/', include('users.urls')),  # כולל את הנתיבים של אפליקציית ה-users
 ]
