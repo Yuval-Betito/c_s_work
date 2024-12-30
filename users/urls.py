f7532b4cd3b84efa2a97ad5adbb7fda735de8bb1
Yuval-Betito <yuval36811@gmail.com>
2024-12-30 16:35:12 +0200

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from users import views  # יש להוסיף את השורה הזו
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # נתיב לפאנל הניהול של Django
